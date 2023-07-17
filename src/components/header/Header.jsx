d3f70376b1d1998f550ba7e0b40ef9844080a5bb
samaragang <dmitr.burii.2016@gmail.com>
2023-07-17 22:05:44 +0300

    header and intro completed

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,34 @@
+import "./header.css"
+import logoImg from './../../images/icons/logo.svg'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__inner">
+          <div className="header__logo">
+            <a href="/">
+              <img src={logoImg} alt="logo" />
+            </a>
+          </div>
+          <div className="header__nav">
+            <div className="nav__item">
+              <a href="/">Авторы</a>
+            </div>
+            <div className="nav__item">
+              <a href="/">Коллекции</a>
+            </div>
+            <div className="nav__item">
+              <a href="/">Сервисы</a>
+            </div>
+          </div>
+          <div className="header__wallet-btn">
+            <button>Кошелёк</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
